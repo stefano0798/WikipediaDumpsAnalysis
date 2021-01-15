cf1bea2ce9be0c9a767c2460a732ed63ba6b66ce
Stefano Facchini <stefano.facchini@studenti.unitn.it>
2021-01-15 02:04:51 +0100

    Score updated, work in progress #2

--- project.py
+++ project.py
@@ -24,22 +24,32 @@ data = df.select(df.page_id, df.rev_id, df.timestamp)
 
 dataset = data.select(data.page_id, data.rev_id, to_timestamp(data.timestamp).alias('timestamp'))
 
-#add current timestamp to compare time difference
+# add current timestamp to compare time difference
 dataset_with_scores = dataset.withColumn("current_timestamp", lit(functions.current_timestamp()))
-#initialize score column to 0
+# initialize score column to 0
 dataset_with_scores = dataset_with_scores.withColumn("score", lit(0))
 
-#transform timestamps to integer in order to retrieve difference (in seconds)
+# transform timestamps to integer in order to retrieve difference (in seconds)
 dataset_with_scores = dataset_with_scores.select(dataset_with_scores.page_id, dataset_with_scores.rev_id, functions.unix_timestamp(dataset_with_scores.timestamp).alias("timestamp"), functions.unix_timestamp(dataset_with_scores.current_timestamp).alias("current_timestamp"), dataset_with_scores.score)
 dataset_with_scores
 dataset_with_scores.show()
 
-#convert seconds to seconds (integer format)
+# convert seconds to seconds (integer format)
 dataset_with_scores = dataset_with_scores.select(dataset_with_scores.page_id, dataset_with_scores.rev_id, functions.round(dataset_with_scores.timestamp).cast('integer').alias("timestamp"), functions.round(dataset_with_scores.current_timestamp).cast('integer').alias("current_timestamp"), dataset_with_scores.score)
 dataset_with_scores
 dataset_with_scores.show()
 
-dataset_with_scores = dataset_with_scores.select(dataset_with_scores.page_id, dataset_with_scores.rev_id, dataset_with_scores.timestamp, dataset_with_scores.current_timestamp, functions.when(dataset_with_scores.current_timestamp - dataset_with_scores.timestamp < 604799, 3).when( ((dataset_with_scores.current_timestamp - dataset_with_scores.timestamp < 2419200) & (dataset_with_scores.current_timestamp - dataset_with_scores.timestamp > 604800)), 1).otherwise(0).alias("score"))
+# calculate score using these values...
+high_score = 3 # points gained for recent edits ( < recent_limit ) (default: 3)
+low_score = 1 # points gained for older edits ( between recent_limit and old_limit) (default: 1)
+recent_limit = 604800 # number of seconds before an edit is considered new (default: 604800, one week)
+old_limit = 2419200 # number of seconds before an edit is still considerable (default: 2419200, one month)
+# in this way, an edit gains three points if it is more recent than one week, while it gains one point if it is between one week and one month old. No points gained for ones older than one month
+dataset_with_scores = dataset_with_scores.select(dataset_with_scores.page_id, dataset_with_scores.rev_id, dataset_with_scores.timestamp, dataset_with_scores.current_timestamp, functions.when(dataset_with_scores.current_timestamp - dataset_with_scores.timestamp <= recent_limit, high_score).when( ((dataset_with_scores.current_timestamp - dataset_with_scores.timestamp <= old_limit) & (dataset_with_scores.current_timestamp - dataset_with_scores.timestamp > recent_limit)), low_score).otherwise(0).alias("score"))
 dataset_with_scores
 dataset_with_scores.show()
 
+#rename for shorter typing...
+data = dataset_with_scores
+
+grouped = data.groupBy("page_id").min("timestamp").show()
